cf86428a7451dbe8a49f87a67618111f7eff8c9e
lqcool2408@gmail.com <lqcool2408@gmail.com>
2024-07-02 21:15:03 +0700

    feat: branchC commit 1

--- Mindx-CIJs90/code-intensive/src/App.js
+++ Mindx-CIJs90/code-intensive/src/App.js
@@ -1,14 +1,16 @@
 // import AppContainer from './lesson12-other-react-hooks/Appcontainer';
 import TaskManager from './task-management-firebase/components/TaskManagement.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+console.log('only branch c has');
+console.log('only branch c has');
+console.log('only branch c has');
+console.log('only branch c has');
+console.log('only branch c has');
+console.log('only branch c has');
+console.log('only branch c has');
+console.log('only branch c has');
 
-console.log('test');
-console.log('test1');
-console.log('test2');
-console.log('test4');
-
-
-var a = 1;
+var a = 4;
 var b = 2;
 
 const App = () => {
